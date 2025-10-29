fbba2984c4d46602a1dee93b950fb82f7718132a
Nissi Sanju <nissiann@.com]>
2025-10-29 02:38:51 +0000

    Intial commit

---
+++ mablibs.py
@@ -0,0 +1,23 @@
+# lets print a welcome message to our user
+print("Welcome to our mad libs generator!\n")
+#the backslah n charcter give a new line
+
+#variables used in the story which will be given by the user
+name1 = input("Enter a name: ")
+place1 = input("Enter a place: ")
+object1 = input("Enter an object: ")
+adjective1 = input("Enter an adjective: ")
+animal1 = input("Enter an animal: ")
+object2 = input("Enter another object: ")
+verb1 = input("Enter a verb ending in -ed: ")
+verb2 = input("Enter a present tense verb: ")
+adverb1 = input("Enter an adverb (-ly): ")
+verb3 = input("Enter a past tense verb: ")
+
+#to give it spacing
+print()
+
+#Make your own story or use mine!
+#vairables are in the curly brackets
+print(f"One dark night, {name1} went into the {place1} searching for a {object1}. Suddenly, a {adjective1} {animal1} appeared holding a {object2}. {name1} was so startled that he or she {verb1} herself or himself. And if that wasn't enough, the {animal1} tried to {verb2} his or her head. {name1} and the {animal1} eventually had a huge fight and tried to resolve their conflicts but the {animal1} {adverb1} {verb3}.")
+
